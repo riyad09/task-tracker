33dbdd74d85a9aefbba73d2c5fab6b62e2db5001
rashadulalam <riyad2006@gmail.com>
2022-01-19 23:22:27 +0600

    added mock data

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'First task',
+    day: 'Jan 20 at 2 pm',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: '2nd task',
+    day: 'Jan 21 at 2 pm',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: 'Third task',
+    day: 'Jan 22 at 2 pm',
+    reminder: true
+  }
+]
