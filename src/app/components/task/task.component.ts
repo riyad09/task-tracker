8316cfc407e5be42759aaac35ca2e11b5f655062
rashadulalam <riyad2006@gmail.com>
2022-01-22 22:41:06 +0600

    imported tasks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mock-task';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-task',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
+  tasks: Task[] = TASKS;
   constructor() { }
 
   ngOnInit(): void {
