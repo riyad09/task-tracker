d73227b2df43df380a43a0e5d782f59f5b1055bd
rashadulalam <riyad2006@gmail.com>
2022-01-23 17:00:08 +0600

    added delete icon

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -8,6 +10,7 @@ import { Task } from 'src/app/Task';
 export class TaskItemComponent implements OnInit {
   @Input()
   task!: Task;
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
